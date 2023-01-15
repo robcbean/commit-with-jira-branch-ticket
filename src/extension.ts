ee1b563fd9ff70e4cf6af0866cc9a123bc50562e
Roberto Beán <robcbean@gmail.com>
2023-01-15 17:36:27 +0100

    first test

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,40 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import simpleGit, {SimpleGit} from 'simple-git';
+import { notEqual } from 'assert';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
+	
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "commit-with-jira-branch-ticket" is now active!');
+
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('commit-with-jira-branch-ticket.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Commit with Jira Branch Ticket!');
+
+		
+		var currentProjectDirectory:string = String(vscode.workspace.rootPath);
+	
+		console.log(`Project directory ${currentProjectDirectory}`);	
+		console.log(`Current working directory ${process.cwd()}`);
+	
+		process.chdir(currentProjectDirectory);
+		console.log(`Current working directory after chdir ${process.cwd()}`);
+
+		var git = simpleGit();
+		var currentBranch = '';
+		async function getCurentBranch(){
+			currentBranch = (await git.branch()).current;
+		}
+		getCurentBranch();
+		console.log(`Current branch :${currentBranch}..`);
+	
 	});
 
 	context.subscriptions.push(disposable);
